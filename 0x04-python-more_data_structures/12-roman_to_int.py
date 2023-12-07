26573b541d4ff6a38f5cba7a183959ce9d882e85
Turner-Baba <sheleogunsemi@gmail.com>
2023-12-07 12:08:39 -0800

    Converts roman numeral to integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    rmdict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    indec = [rmdict[num] for num in roman_string]
+    fig = 0
+    for dig in range(len(indec)):
+        fig += indec[dig]
+        if indec[dig - 1] < indec[dig] and dig != 0:
+            fig -= (indec[dig - 1] + indec[dig - 1])
+    return fig
