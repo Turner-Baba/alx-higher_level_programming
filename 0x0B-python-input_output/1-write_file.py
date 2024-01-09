e3af385857f821911d90a88c3e02708bd785d060
Turner-Baba <sheleogunsemi@gmail.com>
2024-01-09 15:22:47 -0800

    A function that writes a string to a text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""a function that writes a string to a text file (UTF8) and returns the number of characters written"""
+"""A function that writes a string to a text file (UTF8) and
+returns the number of characters written"""
+
 
 def write_file(filename="", text=""):
     """writes a string to a UTF8 text file
     Args:
     filename (str): name of the file to write
     text (str): text to write into the file"""
-    with open(filename, "w", encoding="utf-8") as a:
-        return a.write(text)
+    with open(filename, "w", encoding="utf-8") as fl:
+        return fl.write(text)
